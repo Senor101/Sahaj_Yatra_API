c30b345fc6ac58d5f5eac624910dac601087349e
Senor101 <adikshyanta456@gmail.com>
2024-01-15 13:17:39 +0545

    cors configuration

--- src/api/v1/api.ts
+++ src/api/v1/api.ts
@@ -8,7 +8,7 @@ import transactionRouter from "./routes/transaction.router";
 const router = Router();
 
 router.get("/",(req:Request,res:Response) : Response=> {
-    return res.status(200).send("<h1 align='center'>Welcome to HAMROBUS_API</h1>");
+    return res.status(200).send("<h1 align='center'>Welcome to Sahaj Yatra API</h1>");
 });
 
 router.use('/auth',authRouter);
--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,28 @@ const app = express();
 import apiRouter from './api/v1/api'
 
 app.use(helmet());
-app.use(cors());
+
+const allowedOrigin = ['http://localhost:3000']
+
+var corsOptions = {
+    origin: function (origin:any, callback:any) {
+        if (allowedOrigin.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(null, false)
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    preflightcontinue: true
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1",apiRouter);
+app.use("/api/v1", apiRouter);
 
 // app.use(notFound);
 
