18599662844b9b2d5d053b3d3b7df10dbe7e3f61
Senor101 <adikshyanta456@gmail.com>
2024-01-30 20:36:25 +0545

    customer error handler and not found linked

--- src/app.ts
+++ src/app.ts
@@ -5,32 +5,35 @@ require('dotenv').config();
 
 const app = express();
 
-import apiRouter from './api/v1/api'
+import apiRouter from './api/v1/api';
+import customErrorHandler from './api/v1/middleware/errorhandler.middleware';
 
 app.use(helmet());
 
-const allowedOrigin = ['http://localhost:3000']
+const allowedOrigin = ['http://localhost:3000'];
 
 var corsOptions = {
-    origin: function (origin:any, callback:any) {
+    origin: function (origin: any, callback: any) {
         if (allowedOrigin.indexOf(origin) !== -1) {
-            callback(null, true)
+            callback(null, true);
         } else {
-            callback(null, false)
+            callback(null, false);
         }
     },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
-    preflightcontinue: true
-}
+    preflightcontinue: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", apiRouter);
+app.use('/api/v1', apiRouter);
 
-// app.use(notFound);
+app.use('*', async (req, res) => res.status(404).json({ error: 'not found' }));
+
+app.use(customErrorHandler);
 
 export default app;
