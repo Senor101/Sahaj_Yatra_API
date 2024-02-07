3cbe247fb6405539c58d6c647e1f08732ca80a5d
Senor101 <adikshyanta456@gmail.com>
2024-02-07 22:21:07 +0545

    chore: keep server live for render

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import http from "http";
-require("dotenv").config();
+import http from 'http';
+require('dotenv').config();
 
-import app from "./app"
-import {connectDB} from "./api/v1/config/db.config"
+import app from './app';
+import { connectDB } from './api/v1/config/db.config';
+import User from './api/v1/models/user.model';
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-const startServer = async() => {
+const keepServerLive = async () => {
+    const request = await fetch('http://localhost:8000/api/v1/user');
+};
+
+const startServer = async () => {
     await connectDB();
+    // KEEPING THE SERVER ALIVE TO PREVENT INSTANCE FROM GOING DOWN
+    if (process.env.ENVIRONMENT === 'PRODUCTION') {
+        setInterval(keepServerLive, 10000);
+    }
     server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
