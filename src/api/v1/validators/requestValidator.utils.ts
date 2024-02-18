fc271138ef8480642c6a14583298f206100cc67b
Senor101 <adikshyanta456@gmail.com>
2024-02-18 20:53:00 +0545

    feat: validators

---
+++ src/api/v1/validators/requestValidator.utils.ts
@@ -0,0 +1,18 @@
+import { body } from "express-validator";
+
+const validateBusRegister = [
+    body("busNumber").notEmpty().withMessage("Bus number is required"),
+    body("busType").notEmpty().withMessage("Bus type is required"),
+    body("busRoute").optional(),
+    body("busSeats").optional().isNumeric().withMessage("Bus seats must be a number")
+];
+
+const validateUserRegister = [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").notEmpty().isEmail().withMessage("Email is required"),
+    body("phoneNumber").notEmpty().isMobilePhone("ne-NP").withMessage("Phone number is required"),
+    body("citizenshipNumber").notEmpty().withMessage("Citizenship number is required"),
+    body("password").isLength({ min: 5 }).isAlphanumeric().withMessage("Password must be alphanumeric and at least 5 characters long")
+];
+
+export { validateBusRegister, validateUserRegister };
